aa06eccad8d77472f718250f2b151cce3bd7e193
ghost-ice <fish_youdao@163.com>
2017-06-20 19:42:06 +0800

    font1

--- js/rpg_windows.js
+++ js/rpg_windows.js
@@ -42,7 +42,7 @@ Window_Base.prototype.standardFontFace = function() {
     } else if ($gameSystem.isKorean()) {
         return 'Dotum, AppleGothic, sans-serif';
     } else {
-        return 'Microsoft YaHei';
+        return '微软雅黑';
     }
 };
 
